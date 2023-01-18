673a9f81f7a92b891327f8ece0ff307a01819573
Juni Kim <junikimm717@gmail.com>
2023-01-18 11:58:51 -0500

    overflow protection

--- backend/database/scoring.go
+++ backend/database/scoring.go
@@ -11,6 +11,7 @@ import (
 const (
 	// contests before this year should be penalized
 	THRESHOLD = 2010
+	EXPONENT = 40
 )
 
 func problemScore(p scrape.Problem) float32 {
@@ -18,8 +19,12 @@ func problemScore(p scrape.Problem) float32 {
 	num := re.Find([]byte(p.Source))
 	if num != nil {
 		res, _ := strconv.ParseFloat(string(num), 64)
+		// protect against overflow
+		difference := THRESHOLD - res;
+		difference = math.Max(difference, -EXPONENT);
+		difference = math.Min(difference, EXPONENT);
 		return float32(1.0/(1.0 + math.Exp(THRESHOLD - res)))
 	} else {
-		return 0.1
+		return float32(1.0/(1.0 + math.Exp(EXPONENT)))
 	}
 }
