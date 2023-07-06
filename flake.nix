ba7c4f16196cbd88e35f596ff8871df28ff66d94
Juni Kim <junikimm717@gmail.com>
2023-07-06 10:37:32 -0400

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Search.MAATester.com";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
